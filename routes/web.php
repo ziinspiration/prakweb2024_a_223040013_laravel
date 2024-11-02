78011f328edb56e4216984ac1e69cbfed6638445
ziinspiration <ramddbgk@gmail.com>
2024-11-03 06:09:49 +0700

    Vidio 8 - Laravel - Eloquent ORM & Post Model

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use App\Models\Post;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
-
-
 Route::get('/', function () {
     return view('home', ['title' => 'Home']);
 });
@@ -18,10 +16,7 @@ Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
 });
 
-Route::get('/posts/{slug}', function ($slug) {
-
-    $post = Post::find($slug);
-
+Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
