40ce13af804808876a83af15e8b94ce2671e5b7e
ziinspiration <ramddbgk@gmail.com>
2024-11-03 02:48:50 +0700

    Vidio 2 - Laravel - Struktur Folder

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Blog</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog">Blog</a>
+    <a href="/contact">Contact</a>
+    <h1>Halaman Blog</h1>
+    <article>
+        <div class="article-child">
+            <h3 class="judul">PHP</h3>
+            <p class="isi">PHP adalah bahasa pemrograman open-source yang digunakan untuk membuat aplikasi web dinamis dan interaktif. PHP merupakan singkatan dari Hypertext Preprocessor, atau dalam bahasa Indonesia, Pemrosesan Hiperteks. </p>
+        </div>
+        <div class="article-child">
+            <h3 class="judul">Laravel</h3>
+            <p class="isi">Laravel merupakan framework PHP yang open-source dan berisi banyak modul dasar untuk mengoptimalkan kinerja PHP dalam pengembangan aplikasi web, apalagi PHP adalah bahasa pemrograman yang dinamis dan Laravel disini bisa bertindak untuk membuat web development lebih cepat, lebih aman, dan lebih simpel.</p>
+        </div>
+    </article>
+
+    <script src="js/script.js"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', ['nama' => 'Ilham Ramadhana Hartono']);
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
