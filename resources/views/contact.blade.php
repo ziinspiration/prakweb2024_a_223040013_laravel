a210b77edbefebcb20f787ffafd7112f390c9b50
ziinspiration <ramddbgk@gmail.com>
2024-11-03 04:02:24 +0700

    Vidio 4 - Laravel - Blade Component

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,24 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Halaman Contact</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/blog">Blog</a>
-    <a href="/contact">Contact</a>
-    <h1>Halaman Contact</h1>
-    <ul>
-        <li>Instagram <a href="https://www.instagram.com/ilhamramadhanahartono/">ilhamramadhanahartono</a></li>
-        <li>Twitter <a href="https://x.com/ziimpossible">ziimpossible</a></li>
-        <li>Github <a href="https://github.com/ziinspiration">ziinspiration</a></li>
-    </ul>
-
-    <script src="js/script.js"></script>
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Welcome to Contact !</h3>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Home']);
 });
 
 Route::get('/about', function () {
-    return view('about', ['nama' => 'Ilham Ramadhana Hartono']);
+    return view('about', ['name' => 'Ilham Ramadhana Hartono', 'title' => 'About']);
 });
 
 Route::get('/blog', function () {
-    return view('blog');
+    return view('blog', ['title' => 'Blog']);
 });
 
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact', ['title' => 'Contact']);
 });
