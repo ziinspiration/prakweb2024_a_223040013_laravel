9ba8ec9b8a113769dc330f3f76611dc42e9c7b5d
ziinspiration <ramddbgk@gmail.com>
2024-11-05 05:38:45 +0700

    Vidio 15 - Laravel - Searching

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,14 +1,5 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
-    {{-- <article class="py-8 max-w-screen-md">
-        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
-        <div>
-            By <a class="hover:underline text-base text-gray-500" href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="hover:underline text-base text-gray-500">{{ $post->category->name }}</a> | {{ $post->created_at->diffForHumans() }}
-         </div>
-        <p class="my-4 font-light">{{ $post['body'] }}</p>
-        <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo; Back to Blog</a>
-    </article> --}}
-
 <main class="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900 antialiased">
     <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
         <article class="mx-auto w-full max-w-4xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
@@ -18,9 +9,9 @@
                     <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
                         <img class="mr-4 w-16 h-16 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-2.jpg" alt="{{ $post->author->name }}">
                         <div>
-                            <a href="/authors/{{ $post->author->username }}" rel="author" class="text-xl font-bold text-gray-900 dark:text-white">{{ $post->author->name }}</a>
+                            <a href="/posts?author={{ $post->author->username }}{{ $post->author->username }}" rel="author" class="text-xl font-bold text-gray-900 dark:text-white">{{ $post->author->name }}</a>
                             <p class="text-base mb-1 text-gray-500 dark:text-gray-400">{{ $post->created_at->diffForHumans() }}</p>
-                            <a href="/categories/{{ $post->category->slug }}"><span class="bg-{{ $post->category->color }}-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                            <a href="/posts?category={{ $post->category->slug }}"><span class="bg-{{ $post->category->color }}-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
                                 {{ $post->category->name }}
                             </span></a>
                         </div>
--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,7 @@ Route::get('/posts', function () {
     // $posts = Post::with(['author', 'category'])->latest()->get();
     // return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 
-    $posts = Post::latest()->get();
-    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
+    return view('posts', ['title' => 'Blog', 'posts' => Post::filter(request(['search', 'category', 'author']))->latest()->get()]);
 });
 
 Route::get('/posts/{post:slug}', function (Post $post) {
