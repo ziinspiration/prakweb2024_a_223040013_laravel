60a51ff49fb66ea21fbf66791491bb4875f568a9
ziinspiration <ramddbgk@gmail.com>
2024-11-05 05:50:05 +0700

    Vidio 16 - Laravel - Pagination

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -27,7 +27,9 @@
         </div>
     </div>
 
-    <div class="py-4 px-4 mx-auto max-w-screen-xl lg:py-8 lg:px-0">
+    {{ $posts->links() }}
+
+    <div class="my-4 py-4 px-4 mx-auto max-w-screen-xl lg:py-8 lg:px-0">
             <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
              @forelse ($posts as $post)
                 <article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
@@ -64,4 +66,7 @@
                 @endforelse
             </div>
         </div>
-</x-layout>
+
+        {{ $posts->links() }}
+
+    </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/posts', function () {
     // $posts = Post::with(['author', 'category'])->latest()->get();
     // return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 
-    return view('posts', ['title' => 'Blog', 'posts' => Post::filter(request(['search', 'category', 'author']))->latest()->get()]);
+    return view('posts', ['title' => 'Blog', 'posts' => Post::filter(request(['search', 'category', 'author']))->latest()->paginate(9)->withQueryString()]);
 });
 
 Route::get('/posts/{post:slug}', function (Post $post) {
